25f3b962a1dd0a753eb192ee1414b6685c9e7e3d
Malika Bano <mtmalikatarannum@gmail.com>
2023-03-22 06:04:11 -0700

    increment

---
+++ js/cons.js
@@ -0,0 +1,7 @@
+// const x = 100;
+// const y = 50;
+// const z = x + y; 
+// console.log(z);
+const PI = 3.141592653589793;
+PI = 3.14;      // This will give an error
+PI = PI + 10;   // This will also give an error
---
+++ js/first.js
@@ -0,0 +1,4 @@
+let x = 100;
+let y = 50;
+let z = x + y; 
+console.log(z);
