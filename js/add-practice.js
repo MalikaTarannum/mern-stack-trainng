a92a3827654cb4b6e28fa671557b9d2a8df19331
Malika Bano <mtmalikatarannum@gmail.com>
2023-03-24 10:56:09 -0700

    sub-practice file

--- js/add-practice.js
+++ js/add-practice.js
@@ -1,3 +1,7 @@
+// const assignment1 = 20;
+// const assignment2 = 30;
+// const assignment3 = assignment1 + assignment2;
+// console.log(assignment3);
 let assignment1 = 20;
 let assignment2 = 30;
 let assignment3 = assignment1 + assignment2;
