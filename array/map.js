83970ed550ccee89452f9c0c97a06d3992bb1779
Malika Bano <mtmalikatarannum@gmail.com>
2023-03-27 02:51:04 -0700

    map and reduce file

--- array/map.js
+++ array/map.js
@@ -1,3 +1,3 @@
-let myarray = [1, 2, 3, 4, 5];
+let myarray = [7, 2, 3, 4, 5];
 array = myarray.map((n) => n * 2);
-console.log();
+console.log(array);
---
+++ array/reduce.js
@@ -0,0 +1,3 @@
+let myarray = [1, 2, 3, 4, 5];
+array = myarray.reduce((acc ,cur) => acc + cur);
+console.log(array);
